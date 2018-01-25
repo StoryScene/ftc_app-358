5d8dccd1c75142bac06011da06354765de9e8472
Lawrence <DenouementX@users.noreply.github.com>
2018-01-24 18:24:31 -0800

    updates to exp drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vuMark_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vuMark_Test.java
@@ -14,6 +14,7 @@ public class vuMark_Test {
 
     static void vuMarkCode(VuforiaLocalizer vuforia, int cameraMonitorViewId){
 
+        /**
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         parameters.vuforiaLicenseKey = "AXzW9CD/////AAAAGTPAtr9HRUXZmowtd9p0AUwuXiBVONS/c5x1q8OvjMrQ8/XJGxEp0TP9Kl8PvqSzeXOWIvVa3AeB6MyAQboyW/Pgd/c4a4U/VBs1ouUsVBkEdbaq1iY7RR0cjYr3eLwEt6tmI37Ugbwrd5gmxYvOBQkGqzpbg2U2bVLycc5PkOixu7PqPqaINGZYSlvUzEMAenLOCxZFpsayuCPRbWz6Z9UJfLeAbfAPmmDYoKNXRFll8/jp5Ie7iAhSQgfFggWwyiqMRCFA3GPTsOJS4H1tSiGlMjVzbJnkusPKXfJ0dK3OH9u7ox9ESpi91T0MemXw3nn+/6QRvjGtgFH+wMDuQX7ta89+yW+wqdXX9ZQu8BzY";
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
@@ -21,6 +22,7 @@ public class vuMark_Test {
         VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
         VuforiaTrackable relicTemplate = relicTrackables.get(0);
         relicTemplate.setName("relicVuMarkTemplate");
+         */
 
 
     }
