d76d761fe9b88ff26ceeca7253984bae05c66f4b
kevinwang <PhosphorusElement15@gmail.com>
2018-09-04 16:25:41 -0700

    Oh no

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Whatever_You_Want.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Whatever_You_Want.java
@@ -2,9 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by kevinwang on 8/24/18.
@@ -32,6 +30,7 @@ public class Whatever_You_Want extends LinearOpMode{
 
         waitForStart();
 
+        /*
         //time not yet set
         Whatever_Drive_Code.Drive(FL, BL, FR, BR, 1, 0.8);
         sleep(1200);
@@ -55,5 +54,12 @@ public class Whatever_You_Want extends LinearOpMode{
         sleep(2400);
 
         Whatever_Drive_Code.Drive(FL, BL, FR, BR, 0, 0.8);
+        */
+
+        FL.setPower(0.8);
+        BL.setPower(0.8);
+        FR.setPower(0.8);
+        BR.setPower(0.8);
+        sleep(1000);
     }
 }
